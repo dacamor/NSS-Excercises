f86fa8b867dd5f745e270c6cc85b303ebfdc3570
Penny Caudle <pennycaudle@Pennys-MacBook-Pro.local>
2017-10-24 13:50:27 -0500

    added coins_to_cash function

--- coins_to_cash/cash_to_coins.js
+++ coins_to_cash/cash_to_coins.js
@@ -1,22 +1,28 @@
 const dollarAmount = 0.10
-const piggyBank = {quarters: 0,	dimes: 0, nickels: 0, pennies: 0}
+const cash_to_coins = function (dollarAmount) {
+	const piggyBank = { quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
 
 
-  while (dollarAmount >= .25){
-	  piggyBank.quarters++
-	  dollarAmount -= .25
-  }
-while (dollarAmount >= .10){
-	piggyBank.dimes++
-	dollarAmount -= .10
-}
-while (dollarAmount >= .05){
-	piggyBank.nickels++
-	dollarAmount -= .05
-}
-while (dollarAmount >= .01){
-	piggyBank.pennies++
-	dollarAmount -= .01
+	while (dollarAmount >= .25) {
+		piggyBank.quarters++
+		dollarAmount -= .25
+	}
+	while (dollarAmount >= .10) {
+		piggyBank.dimes++
+		dollarAmount -= .10
+	}
+	while (dollarAmount >= .05) {
+		piggyBank.nickels++
+		dollarAmount -= .05
+	}
+	while (dollarAmount >= .01) {
+		piggyBank.pennies++
+		dollarAmount -= .01
+	}
+
+	console.log(piggyBank)
 }
 
-console.log(piggyBank)
+const coins_to_cash = function (coins){
+	return (coins.pennies*.01 + coins.dimes*.1 + coins.nickels*.05 + coins.quarters*.25)
+}
